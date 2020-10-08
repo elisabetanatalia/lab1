952fb31ac817d8ed0f7db354ad6db706800a8dbe
elisabetanatalia <72567224+elisabetanatalia@users.noreply.github.com>
2020-10-08 19:34:50 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,28 +1,29 @@
-var video = document.getElementById("video");
-
-//---------
-
-function on_cam_success(stream) {
-    video.srcObject = stream;
+var video=document.getElementById("video");
+//------------
+function on_cam_success(stream)
+{
+    video.srcObject=stream;
 }
-
-function on_cam_error(err) {
-    alert("error." + err.message);
+//-----------
+function on_cam_error(err)
+{
+    alert("error."+err.message);
 }
-
-//----------
-
-var constraints = {audio: false, video: true};
+//-------------
+var constraints={audio:false,video:true};
 navigator.mediaDevices.getUserMedia(constraints)
 .then(on_cam_success)
 .catch(on_cam_error);
-
-//----------
-
-function capteaza() {
-    var c = document.getElementById("canvas");
-    c.width = video.width;
-    c.height = video.height;
-    var ctx = c.getContext("2d");
+//--------------
+function capteaza()
+{
+    var c=document.getElementById("canvas");
+    c.width=video.width;
+    c.height=video.height;
+    var ctx=c.getContext("2d");
     ctx.drawImage(video,0,0,640,480);
+   
 }
+
+video.addEventListener("touchstart", capteaza);
+video.addEventListener("mousedown", capteaza);
